35f52582fbfb2a376e7e3c91deb63012b24f5f09
Soroush Sabet <ssabet@protonmail.com>
2023-10-16 08:49:31 +0100

    update create_sysimage file to remove the replace_default=true option

--- create_sysimage.jl
+++ create_sysimage.jl
@@ -1,5 +1,6 @@
 using PackageCompiler
 create_sysimage([:Pluto, :PlutoUI, :PlotlyLight, :LaTeXStrings];
+                sysimage_path="sys_plots.so",
                 precompile_execution_file = "warmup.jl",
-                replace_default = true,
+                #replace_default = true,
                 cpu_target = PackageCompiler.default_app_cpu_target())
--- runpluto.sh
+++ runpluto.sh
@@ -18,4 +18,4 @@ Pluto.run(
     require_secret_for_access=false
 )"
 echo ${command}
-julia --project="/home/jovyan" --optimize=0 -e "${command}"
+julia --sysimage sys_plots.so --project="/home/jovyan" --optimize=0 -e "${command}"
